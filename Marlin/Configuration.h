3552875c52aa746faccda7065021976616dc2fcf
Sekisback <sek_is_back@gmx.de>
2021-02-24 19:36:06 +0100

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -496,9 +496,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  27.55
-    #define DEFAULT_Ki   2.64
-    #define DEFAULT_Kd  71.77
+    #define DEFAULT_Kp  21.73
+    #define DEFAULT_Ki   1.54
+    #define DEFAULT_Kd  76.55
   #endif
 #endif // PIDTEMP
 
