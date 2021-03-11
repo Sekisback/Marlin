50161fec0df79e82b7c979e56a139deaa013be83
Sekisback <sek_is_back@gmx.de>
2021-03-11 16:52:17 +0100

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -72,7 +72,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(SEK, BLTouch)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(SEK, Wizzard)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -133,7 +133,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Printer"
+#define CUSTOM_MACHINE_NAME "Printer V1.2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
